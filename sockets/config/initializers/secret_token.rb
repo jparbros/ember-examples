50aa5ff303c1713c56af9e64e8a5b6dba8565d3b
Jorge Pardiñas <jparbros@gmail.com>
2013-11-28 12:19:29 -0600

    First commit - Emberjs and sockets

---
+++ sockets/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sockets::Application.config.secret_key_base = '26ced408fe84f091014b21b7d466790e323f4bfd53d6219954db5eee129321559146f5600f3aade407d07a0c8ab5b0e629d7d2d1d6e3a293770eecd29b507c9c'
